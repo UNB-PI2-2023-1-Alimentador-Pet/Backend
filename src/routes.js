0da477797b2f3ceb9dcdc36778caf6c3988de58d
MaiconMares <maiconmaresunb@gmail.com>
2023-05-19 16:41:22 -0300

    Retrieving data from database.

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const UserController = require("./controllers/UserController");
 const routes = Router();
 
 routes.get("/", UserController.sayHi);
-routes.post("/user/new", UserController.createUser)
+routes.post("/user/new", UserController.createUser);
+routes.get("/users", UserController.fetchUsers);
 
 module.exports = routes;
